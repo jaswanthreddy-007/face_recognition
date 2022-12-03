cc91539ae845c9ed6d5fc9ec186a93c47ab8fff8
jaswanthreddy-007 <88081034+jaswanthreddy-007@users.noreply.github.com>
2022-12-04 00:24:00 +0530

    local database detection

--- face_rec.py
+++ face_rec.py
@@ -1,22 +1,61 @@
-import cv2 as cv
+import cv2
 import numpy as np
 import face_recognition
+import os
+from datetime import datetime
+from PIL import ImageGrab
+
+path = 'C:/Users/JaswanthReddy/Hackathon/face_recognition/images'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
 
 
-imgelon = face_recognition.load_image_file('C:/Users/JaswanthReddy/Hackathon/face_recognition/Elon_Musk_2015.jpg')
-imgelon = cv.cvtColor(imgelon,cv.COLOR_BGR2RGB)
-imgtest = face_recognition.load_image_file('C:/Users/JaswanthReddy/Hackathon/face_recognition/taher.jpegg')
-imgtest = cv.cvtColor(imgtest,cv.COLOR_BGR2RGB)
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
 
-faceloc = face_recognition.face_locations(imgelon)[0]
-encodeelon = face_recognition.face_encodings(imgelon)[0]
-print(faceloc)
 
-encodetest = face_recognition.face_encodings(imgtest)[0]
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
 
-results = face_recognition.compare_faces([encodeelon],encodetest)
-print(results)
-cv.imshow('elon',imgelon)
-cv.imshow('test',imgtest)
-cv.waitKey(0)
-cv.destroyAllWindows()
+cap = cv2.VideoCapture(0)
+
+
+img = cv2.imread(
+    'C:/Users/JaswanthReddy/Hackathon/face_recognition/Elon_Musk_2015.jpg')
+#img = captureScreen()
+imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+facesCurFrame = face_recognition.face_locations(imgS)
+encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+    matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+    faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+    #print(faceDis)
+    matchIndex = np.argmin(faceDis)
+
+    if matches[matchIndex]:
+        name = classNames[matchIndex].upper()
+        #print(name)
+        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, name, (x1+6, y2-6),
+                    cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+        cv2.imshow('img', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
